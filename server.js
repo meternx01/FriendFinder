5312454d6f285f76ccce40ddf13100bc49b241b6
Jason McKinney <meter_1@yahoo.com>
2017-09-23 15:44:32 -0500

    server.js additions
    added dependencies, initiated express, added routes, and started listener

---
+++ server.js
@@ -0,0 +1,40 @@
+// FriendFinder Homework Assignment
+// by Jason Mckinney
+// 9/26/2017
+//
+// Implements a express.js server to create a simple matchmaking app
+// where users answer questions and are matched to one another
+// accesses a MySQL database that contains the user's information.
+//------------------------------------------------------------------------------
+// This code is the main server to listen and begin to route the requests to the correct
+// sections of code
+//
+// FLOW *******
+//	HTML ROUTES /*/*/*/*	
+// 	home.html --LINKS-TO--> survey.html --POST-TO-API--> modal with match
+// 	
+//	API ROUTEs */*/*/*
+//	GET -> /api/friends
+//		returns all possible friends
+//	POST -> /api/friends
+//		reciever for survey information
+//
+//------------------------------------------------------------------------------
+
+var express =require("express");
+var bodyParser = require("body-parser");
+
+var app = express();
+var PORT process.env.PORT || 8080;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+require(".routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+app.listen(PORT, function(){
+	console.log("App running on PORT:", PORT);
+})
