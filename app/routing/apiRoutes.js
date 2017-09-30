c0870191ccc29cf6ed009a683874604f8f6fb0d0
Jason McKinney <meter_1@yahoo.com>
2017-09-30 14:07:25 -0500

    restored queres

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,23 +9,23 @@ module.exports = function(app) {
     });
 
     app.get("/api/match", function(req, res) {
-        console.log("Entered Match Route",req.query.score);
-        // connection.query("SELECT * FROM friends WHERE totalScore>0 ORDER BY ABS(totalScore - ? ) ASC LIMIT 1",req.body, function(err, results){
-        //   res.json(results);
-        // });
-		res.json({ name : "Hulky",
-        photo : "http://cdn.sportsmemorabilia.com/sports-product-image/1-t5118366-500.jpg",
-        scores : "12345",
-        totalScore: 35});
+//        console.log("Entered Match Route",req.query.score);
+         connection.query("SELECT * FROM friends WHERE totalScore>0 ORDER BY ABS(totalScore - ? ) ASC LIMIT 1",req.body, function(err, results){
+           res.json(results);
+         });
+//		res.json({ name : "Hulky",
+//        photo : "http://cdn.sportsmemorabilia.com/sports-product-image/1-t5118366-500.jpg",
+//        scores : "12345",
+//        totalScore: 35});
     });
 
     app.post("/api/friends", function(req, res) {
-        console.log("Entered POST", req.body);
-//        var query = connection.query("INSERT INTO friends SET ?", req.body, function(error, results, fields){
-//            if(error) throw error;
-//            console.log(results.affectedRows + " friend inserted!\n");
+//        console.log("Entered POST", req.body);
+        var query = connection.query("INSERT INTO friends SET ?", req.body, function(error, results, fields){
+            if(error) throw error;
+            console.log(results.affectedRows + " friend inserted!\n");
 			res.end();
-//        });
-//        console.log(query.sql);
+        });
+        console.log(query.sql);
     });
 }
